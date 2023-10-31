a619ca0880d7700959c892fff61ab17b6f7c9d47
Gohar Ali <ghulammujtaba074@gmail.com>
2023-10-31 12:23:48 +0500

    itsme

---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+import React from "react";
+function Car() {
+    return <h2>Hi, I am Header</h2>;
+  }
+
+export default Car;
---
+++ src/components/HeroSection.js
@@ -0,0 +1,6 @@
+import React from "react";
+function Hero() {
+    return <h2>Hi, I am Hero</h2>;
+  }
+
+export default Hero;
